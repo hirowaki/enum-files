cf988e56d7b51f8c91eebc21a33c3e04eac201f1
hirowaki <hirowaki0314@gmail.com>
2017-05-31 23:29:38 +0900

    test coverage 100%

--- test/enumFilesTest.js
+++ test/enumFilesTest.js
@@ -3,13 +3,56 @@
 const assert = require('assert');
 const sinon = require('sinon');
 const Promise = require('bluebird');
+const fsExtra = require('fs-extra-promise').usePromise(Promise); // using bluebird.
 const EnumFiles = require('./../index');
+
+function createTestFolders() {
+    return removeTestFolders()
+    .then(() => {
+        // build up directories.
+        return Promise.resolve([
+            'test/testFolder',
+            'test/testFolder/test1',
+            'test/testFolder/test2',
+            'test/testFolder/test1/test1_1',
+            'test/testFolder/test1/test1_2'
+        ])
+        .each((dir) => {
+            return fsExtra.ensureDirAsync(dir);
+        })
+    })
+    .then(() => {
+        // build up files.
+        return Promise.resolve([
+            'test/testFolder/test1.txt',
+            'test/testFolder/test2.txt',
+            'test/testFolder/test1/test1.txt',
+            'test/testFolder/test1/test2.txt',
+            'test/testFolder/test1/test1_1/test1.txt',
+            'test/testFolder/test1/test1_1/test2.txt'
+        ])
+        .each((file) => {
+            return fsExtra.outputJson(file, {test: 'test file'});
+        })
+    });
+}
+
+function removeTestFolders() {
+    return fsExtra.removeAsync('test/testFolder');
+}
 
 describe('EnumFiles', function () {
     let sandbox, spyFiles, spyDir;
 
     before(function () {
         sandbox = sinon.sandbox.create();
+
+        // oh my god.
+        return createTestFolders();
+    });
+
+    after(function () {
+        return removeTestFolders();
     });
 
     afterEach(function () {
@@ -20,64 +63,103 @@ describe('EnumFiles', function () {
         beforeEach(function () {
             spyFiles = sandbox.stub(EnumFiles, '_enumFiles');
             spyFiles.callsFake(function () {
-                return Promise.resolve("_enumFiles");
+                return Promise.resolve('_enumFiles');
             });
 
             spyDir = sandbox.stub(EnumFiles, '_enumDir');
             spyDir.callsFake(function () {
-                return Promise.resolve("_enumDir");
+                return Promise.resolve('_enumDir');
             });
         });
 
         it('files.', function () {
-            return EnumFiles.files("TEST PATH")
+            return EnumFiles.files('TEST PATH')
             .then((res) => {
                 // make sure private function was hit as we expected.
                 assert.strictEqual(spyFiles.callCount, 1);
-                assert.deepEqual(spyFiles.args[0], ["TEST PATH", false]);
+                assert.deepEqual(spyFiles.args[0], ['TEST PATH', false]);
                 assert.strictEqual(spyDir.callCount, 0);
 
                 // make sure it returns the result itself coming from the private one.
-                assert.strictEqual(res, "_enumFiles");
+                assert.strictEqual(res, '_enumFiles');
             });
         });
 
         it('filesRecursively.', function () {
-            return EnumFiles.filesRecursively("TEST PATH")
+            return EnumFiles.filesRecursively('TEST PATH')
             .then((res) => {
                 // make sure private function was hit as we expected.
                 assert.strictEqual(spyFiles.callCount, 1);
-                assert.deepEqual(spyFiles.args[0], ["TEST PATH", true]);
+                assert.deepEqual(spyFiles.args[0], ['TEST PATH', true]);
                 assert.strictEqual(spyDir.callCount, 0);
 
                 // make sure it returns the result itself coming from the private one.
-                assert.strictEqual(res, "_enumFiles");
+                assert.strictEqual(res, '_enumFiles');
             });
         });
 
         it('dir.', function () {
-            return EnumFiles.dir("TEST PATH")
+            return EnumFiles.dir('TEST PATH')
             .then((res) => {
                 // make sure private function was hit as we expected.
                 assert.strictEqual(spyDir.callCount, 1);
-                assert.deepEqual(spyDir.args[0], ["TEST PATH", false]);
+                assert.deepEqual(spyDir.args[0], ['TEST PATH', false]);
                 assert.strictEqual(spyFiles.callCount, 0);
 
                 // make sure it returns the result itself coming from the private one.
-                assert.strictEqual(res, "_enumDir");
+                assert.strictEqual(res, '_enumDir');
             });
         });
 
         it('dirRecursively.', function () {
-            return EnumFiles.dirRecursively("TEST PATH")
+            return EnumFiles.dirRecursively('TEST PATH')
             .then((res) => {
                 // make sure private function was hit as we expected.
                 assert.strictEqual(spyDir.callCount, 1);
-                assert.deepEqual(spyDir.args[0], ["TEST PATH", true]);
+                assert.deepEqual(spyDir.args[0], ['TEST PATH', true]);
                 assert.strictEqual(spyFiles.callCount, 0);
 
                 // make sure it returns the result itself coming from the private one.
-                assert.strictEqual(res, "_enumDir");
+                assert.strictEqual(res, '_enumDir');
+            });
+        });
+    });
+
+    describe('_enumAll', function () {
+        it('regular case success.', function () {
+            return EnumFiles._enumAll('test/testFolder')
+            .then((res) => {
+                assert.deepEqual(res, [
+                    'test/testFolder/test1',
+                    'test/testFolder/test1.txt',
+                    'test/testFolder/test2',
+                    'test/testFolder/test2.txt'
+                ]);
+            });
+        });
+
+        it('regular case success against an empty folder.', function () {
+            return EnumFiles._enumAll('test/testFolder/test999')
+            .then((res) => {
+                // It should not throw an exception.
+                // Just an empty array would be expected to come.
+                assert.deepEqual(res, []);
+            });
+        });
+
+        it('readdir gave us an error!', function () {
+            const spyError = sandbox.stub(require('fs'), 'readdir');
+            spyError.callsFake(function (path, cb) {
+                return cb(new Error('TEST ERROR'));
+            });
+
+            return EnumFiles._enumAll('test/testFolder')
+            .then(() => {
+                assert.ok(false);   // should not come here.
+            })
+            .catch((err) => {
+                assert.ok(true);    // should hit this.
+                assert.strictEqual(err.message, 'TEST ERROR');
             });
         });
     });
@@ -85,7 +167,7 @@ describe('EnumFiles', function () {
     describe('_enumDir', function () {
         describe('shallow search', function () {
             it('test/testFolder.', function () {
-                return EnumFiles._enumDir("test/testFolder")
+                return EnumFiles._enumDir('test/testFolder')
                 .then((res) => {
                     assert.deepEqual(res, [
                         'test/testFolder/test1',
@@ -95,7 +177,7 @@ describe('EnumFiles', function () {
             });
 
             it('test/testFolder/test1.', function () {
-                return EnumFiles._enumDir("test/testFolder/test1")
+                return EnumFiles._enumDir('test/testFolder/test1')
                 .then((res) => {
                     assert.deepEqual(res, [
                         'test/testFolder/test1/test1_1',
@@ -105,14 +187,14 @@ describe('EnumFiles', function () {
             });
 
             it('test/testFolder/test1/test1_1.', function () {
-                return EnumFiles._enumDir("test/testFolder/test1/test1_1")
+                return EnumFiles._enumDir('test/testFolder/test1/test1_1')
                 .then((res) => {
                     assert.deepEqual(res, []);
                 });
             });
 
             it('run againt a non-exsist directory.', function () {
-                return EnumFiles._enumDir("test/testFolder/test100")
+                return EnumFiles._enumDir('test/testFolder/test100')
                 .then((res) => {
                     assert.deepEqual(res, []);
                 });
@@ -121,7 +203,7 @@ describe('EnumFiles', function () {
 
         describe('deep search', function () {
             it('test/testFolder.', function () {
-                return EnumFiles._enumDir("test/testFolder", true)
+                return EnumFiles._enumDir('test/testFolder', true)
                 .then((res) => {
                     assert.deepEqual(res, [
                         'test/testFolder/test1',
@@ -133,7 +215,7 @@ describe('EnumFiles', function () {
             });
 
             it('test/testFolder/test1.', function () {
-                return EnumFiles._enumDir("test/testFolder/test1", true)
+                return EnumFiles._enumDir('test/testFolder/test1', true)
                 .then((res) => {
                     assert.deepEqual(res, [
                         'test/testFolder/test1/test1_1',
@@ -143,7 +225,7 @@ describe('EnumFiles', function () {
             });
 
             it('test/testFolder/test1/test1_1.', function () {
-                return EnumFiles._enumDir("test/testFolder/test1/test1_1", true)
+                return EnumFiles._enumDir('test/testFolder/test1/test1_1', true)
                 .then((res) => {
                     assert.deepEqual(res, []);
                 });
@@ -154,7 +236,7 @@ describe('EnumFiles', function () {
     describe('_enumFiles', function () {
         describe('shallow search', function () {
             it('test/testFolder.', function () {
-                return EnumFiles._enumFiles("test/testFolder")
+                return EnumFiles._enumFiles('test/testFolder')
                 .then((res) => {
                     assert.deepEqual(res, [
                         'test/testFolder/test1.txt',
@@ -164,7 +246,7 @@ describe('EnumFiles', function () {
             });
 
             it('test/testFolder/test1.', function () {
-                return EnumFiles._enumFiles("test/testFolder/test1")
+                return EnumFiles._enumFiles('test/testFolder/test1')
                 .then((res) => {
                     assert.deepEqual(res, [
                         'test/testFolder/test1/test1.txt',
@@ -174,7 +256,7 @@ describe('EnumFiles', function () {
             });
 
             it('test/testFolder/test1/test1_1.', function () {
-                return EnumFiles._enumFiles("test/testFolder/test1/test1_1")
+                return EnumFiles._enumFiles('test/testFolder/test1/test1_1')
                 .then((res) => {
                     assert.deepEqual(res, [
                         'test/testFolder/test1/test1_1/test1.txt',
@@ -184,14 +266,14 @@ describe('EnumFiles', function () {
             });
 
             it('test/testFolder/test1/test1_2.', function () {
-                return EnumFiles._enumFiles("test/testFolder/test1/test1_2")
+                return EnumFiles._enumFiles('test/testFolder/test1/test1_2')
                 .then((res) => {
                     assert.deepEqual(res, []);
                 });
             });
 
             it('run againt a non-exsist directory.', function () {
-                return EnumFiles._enumFiles("test/testFolder/test100")
+                return EnumFiles._enumFiles('test/testFolder/test100')
                 .then((res) => {
                     assert.deepEqual(res, []);
                 });
@@ -200,7 +282,7 @@ describe('EnumFiles', function () {
 
         describe('deep search', function () {
             it('test/testFolder.', function () {
-                return EnumFiles._enumFiles("test/testFolder", true)
+                return EnumFiles._enumFiles('test/testFolder', true)
                 .then((res) => {
                     assert.deepEqual(res, [
                         'test/testFolder/test1.txt',
@@ -214,7 +296,7 @@ describe('EnumFiles', function () {
             });
 
             it('test/testFolder/test1.', function () {
-                return EnumFiles._enumFiles("test/testFolder/test1", true)
+                return EnumFiles._enumFiles('test/testFolder/test1', true)
                 .then((res) => {
                     assert.deepEqual(res, [
                         'test/testFolder/test1/test1.txt',
@@ -226,7 +308,7 @@ describe('EnumFiles', function () {
             });
 
             it('test/testFolder/test1/test1_1.', function () {
-                return EnumFiles._enumFiles("test/testFolder/test1/test1_1", true)
+                return EnumFiles._enumFiles('test/testFolder/test1/test1_1', true)
                 .then((res) => {
                     assert.deepEqual(res, [
                         'test/testFolder/test1/test1_1/test1.txt',
@@ -236,7 +318,7 @@ describe('EnumFiles', function () {
             });
 
             it('test/testFolder/test1/test1_2.', function () {
-                return EnumFiles._enumFiles("test/testFolder/test1/test1_2", true)
+                return EnumFiles._enumFiles('test/testFolder/test1/test1_2', true)
                 .then((res) => {
                     assert.deepEqual(res, []);
                 });
